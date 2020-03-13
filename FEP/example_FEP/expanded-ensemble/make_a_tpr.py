ef61e0e458bca3f0ca59bb8d951510be02bf8e35
Vincent Voelz <vvoelz@gmail.com>
2020-03-13 08:40:04 -0400

    added automatic distance calc for pull_coord1_init

--- FEP/example_FEP/expanded-ensemble/make_a_tpr.py
+++ FEP/example_FEP/expanded-ensemble/make_a_tpr.py
@@ -1,4 +1,6 @@
 import os, sys, stat
+import subprocess
+
 from expanded import *
 
 ###############
@@ -13,11 +15,43 @@ grofile = 'npt.gro'
 topfile = 'topol.top'
 ndxfile = 'index.ndx'
 
+# To tether the ligand to the protein, we need to have prepared an index file with
+# the following atom groups, for example:
+"""
+[ a1-Protein ]
+678
+[ a2-Ligand ]
+1564
+[ Restraint-Distance ]
+678 1564
+"""
+# NOTE that for our tool chain to work smoothly, we should always use these DEFAULT 
+# atom group names
+
+
+### To create the mdpfile, we need to know these default names, *and* the distance between the atoms
+
+# write a temp "selection.dat"
+fout = open('selection.dat', 'w')
+fout.write('group "Restraint-Distance"\n')
+fout.close()
+distance_cmd = 'gmx distance -f {grofile} -n {ndxfile} -sf selection.dat'.format(grofile=grofile, ndxfile=ndxfile)
+output = subprocess.check_output(distance_cmd, shell=True).decode("utf-8")
+output_lines = output.split('\n')
+atom_distance = None
+for line in output_lines:
+    if line.count('Average') > 0:
+        fields = line.split()
+        print('fields', fields)
+        atom_distance = float( fields[2] )  # ['Average', 'distance:', '0.387', 'nm']
+print('atom_distance is', atom_distance, 'nm')
+
 # use the expanded_ensemble_mdpfile() class to create an initial mdpfile
-e = expanded_ensemble_mdpfile()
+e = expanded_ensemble_mdpfile(pull_group1_name = 'a1-Protein',
+                        pull_group2_name = 'a2-Ligand',
+                        pull_coord1_init  = atom_distance)
 mdpfile = 'ee.mdp'
 e.write_to_filename(mdpfile)
-
 
 #### make the first round of simulation
 jobdir = 'out1'
