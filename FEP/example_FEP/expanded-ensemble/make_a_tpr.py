eb6942b3e25b3645765b5c66f3bb62280d345436
vvoelz <vvoelz@gmail.com>
2020-03-12 23:45:56 -0400

    now uses expanded class to make initial mdp

--- FEP/example_FEP/expanded-ensemble/make_a_tpr.py
+++ FEP/example_FEP/expanded-ensemble/make_a_tpr.py
@@ -1,22 +1,39 @@
-import os, sys
-import subprocess
+import os, sys, stat
+from expanded import *
 
 ###############
 ## Let's see if grompp can build an expanded ensemble simulaton
 
 os.system('gmx --version')
 
-home  = os.path.curdir
+##################
+
+# Gather all the setup files in this dir
+grofile = 'npt.gro'
+topfile = 'topol.top'
+ndxfile = 'index.ndx'
+
+# use the expanded_ensemble_mdpfile() class to create an initial mdpfile
+e = expanded_ensemble_mdpfile()
+mdpfile = 'ee.mdp'
+e.write_to_filename(mdpfile)
+
+
+#### make the first round of simulation
 jobdir = 'out1'
 if not os.path.exists(jobdir):
     os.mkdir(jobdir)
 
-grofile = 'npt.gro'
-mdpfile = 'ee-umbrella.mdp'
-topfile = 'topol.top'
-ndxfile = 'index.ndx'
-
-
+# grompp to make a tpr
 grompp_cmd = "gmx grompp -c {grofile} -f {mdpfile} -p {topfile} -n {ndxfile} -o {jobdir}/frame0.tpr -po {jobdir}/mdout.mdp -maxwarn 1".format(grofile=grofile, mdpfile=mdpfile, topfile=topfile, ndxfile=ndxfile, jobdir=jobdir)
-print('grompp_cmd:')
+print('>>', grompp_cmd)
 os.system(grompp_cmd)
+
+# write a runme 
+runme_file = os.path.join(jobdir, 'runme')
+fout = open(runme_file, 'w')
+fout.write('gmx mdrun -v -s frame0.tpr\n')
+fout.close()
+os.chmod(runme_file, stat.S_IRWXU)
+
+print('Wrote:', runme_file)
