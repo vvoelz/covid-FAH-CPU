691eae750383fbd4535a011d5c0aa59207278922
Vincent Voelz <vvoelz@gmail.com>
2020-03-16 07:13:10 -0400

    added ligonly option

--- FEP/scripts/make_fep_ready.py
+++ FEP/scripts/make_fep_ready.py
@@ -3,7 +3,9 @@ import numpy as np
 
 
 usage = """
-    Usage:   make_fep_ready.py [input rundir] [output rundir]
+    Usage:   make_fep_ready.py [input rundir] [output rundir] ligonly
+
+    use "ligonly" keyword if you are readying ligand-only simulatons
 
 NOTES
     * This should be used with an installation of GROMACS 5.0.4, as on the FAH servers!!!!
@@ -12,7 +14,8 @@ NOTES
 
 EXAMPLE
     $ python make_fep_ready.py ../100_ligands/RUN1 ./RUN1
-
+or 
+    $ python make_fep_ready.py ../100_ligands_noreceptor/RUN1 ./RUN1 ligonly
 """
 
 if len(sys.argv) < 3:
@@ -24,6 +27,10 @@ in_rundir = sys.argv[1]
 out_rundir = sys.argv[2]
 if not os.path.exists(out_rundir):
     os.mkdir(out_rundir)
+ligand_only = False
+if len(sys.argv) > 3:
+    if sys.argv[3].count('only') > 0:
+        ligand_only = True
 
 
 ############# convert the grofile ###############3
@@ -86,50 +93,52 @@ GMX_BIN = os.environ['GMXBIN']
 cmd = 'echo "q\\n" | {GMX_BIN}/gmx make_ndx -f {out_grofile} -o {ndxfile}'.format(GMX_BIN=GMX_BIN, out_grofile=out_grofile, ndxfile=ndxfile)
 os.system(cmd)
 
-## Finally, we have to select two atoms for the restraint 
+if not ligand_only:
 
-### Pick '  165MET     CA' as the atom on the protein
-gro_lines = [ line for line in gro_contents.split('\n') if line.count('  165MET     CA') > 0 ]
-print('gro_lines', gro_lines)
-if len(gro_lines)  == 0:
-    print("Can't find 165MET     CA in the grofile -- what gives????  Exiting.")
-    sys.exit(1)
-protein_atomnum = int( (gro_lines[0].strip()).split()[2] )
-protein_xyz = np.array( [float(s) for s in (gro_lines[0].strip()).split()[3:]] )   # units nm
-print('protein_atomnum', protein_atomnum)
-print('protein_xyz', protein_xyz)
+    ## Finally, we have to select two atoms for the restraint 
 
-### Pick the closest carbon in the LIG to the protein_atom
-lig_lines = [ line for line in gro_contents.split('\n') if ( (line.count('LIG') > 0) and (line.count(' C')>0) ) ]
-print('lig_lines', lig_lines)
-if len(lig_lines)  == 0:
-    print("Can't find LIG and C in grofile -- what gives????  Exiting.")
-    sys.exit(1)
-ligand_positions = []
-for i in range(len(lig_lines)):
-    ligand_positions.append( [float(s) for s in (lig_lines[i].strip()).split()[3:]] )
-ligand_positions = np.array(ligand_positions)
-print('ligand_positions', ligand_positions)
-ligand_vectors = ligand_positions - np.tile(protein_xyz, (ligand_positions.shape[0],1))  # N, 3
-print('ligand_vectors', ligand_vectors)
-ligand_distances = ligand_vectors[:,0]**2 + ligand_vectors[:,1]**2 +ligand_vectors[:,2]**2  
-print('ligand_distances', ligand_distances)
-i_closest = np.argmin(ligand_distances)
-print('i_closest', i_closest)
-print('lig_lines[i_closest]', lig_lines[i_closest])
-ligand_atomnum = int( (lig_lines[i_closest].strip()).split()[2] )
-print('ligand_atomnum', ligand_atomnum)
-
-fout = open(ndxfile, 'a')  # append these new lines to the index file...
-index_txt = """[ a1-Protein ]
-  {protein_atomnum} 
-[ a2-Ligand ]
-  {ligand_atomnum}
-[ Restraint-Distance ]
-  {protein_atomnum} {ligand_atomnum}
-""".format(protein_atomnum=protein_atomnum, ligand_atomnum=ligand_atomnum)
-fout.write(index_txt)
-fout.close()
+    ### Pick '  165MET     CA' as the atom on the protein
+    gro_lines = [ line for line in gro_contents.split('\n') if line.count('  165MET     CA') > 0 ]
+    print('gro_lines', gro_lines)
+    if len(gro_lines)  == 0:
+        print("Can't find 165MET     CA in the grofile -- what gives????  Exiting.")
+        sys.exit(1)
+    protein_atomnum = int( (gro_lines[0].strip()).split()[2] )
+    protein_xyz = np.array( [float(s) for s in (gro_lines[0].strip()).split()[3:]] )   # units nm
+    print('protein_atomnum', protein_atomnum)
+    print('protein_xyz', protein_xyz)
+    
+    ### Pick the closest carbon in the LIG to the protein_atom
+    lig_lines = [ line for line in gro_contents.split('\n') if ( (line.count('LIG') > 0) and (line.count(' C')>0) ) ]
+    print('lig_lines', lig_lines)
+    if len(lig_lines)  == 0:
+        print("Can't find LIG and C in grofile -- what gives????  Exiting.")
+        sys.exit(1)
+    ligand_positions = []
+    for i in range(len(lig_lines)):
+        ligand_positions.append( [float(s) for s in (lig_lines[i].strip()).split()[3:]] )
+    ligand_positions = np.array(ligand_positions)
+    print('ligand_positions', ligand_positions)
+    ligand_vectors = ligand_positions - np.tile(protein_xyz, (ligand_positions.shape[0],1))  # N, 3
+    print('ligand_vectors', ligand_vectors)
+    ligand_distances = ligand_vectors[:,0]**2 + ligand_vectors[:,1]**2 +ligand_vectors[:,2]**2  
+    print('ligand_distances', ligand_distances)
+    i_closest = np.argmin(ligand_distances)
+    print('i_closest', i_closest)
+    print('lig_lines[i_closest]', lig_lines[i_closest])
+    ligand_atomnum = int( (lig_lines[i_closest].strip()).split()[2] )
+    print('ligand_atomnum', ligand_atomnum)
+   
+    fout = open(ndxfile, 'a')  # append these new lines to the index file...
+    index_txt = """[ a1-Protein ]
+      {protein_atomnum} 
+    [ a2-Ligand ]
+      {ligand_atomnum}
+    [ Restraint-Distance ]
+      {protein_atomnum} {ligand_atomnum}
+    """.format(protein_atomnum=protein_atomnum, ligand_atomnum=ligand_atomnum)
+    fout.write(index_txt)
+    fout.close()
 
 
 ### do a very quick minimization to make sure we're all set to run
@@ -148,7 +157,7 @@ define                   =
 integrator               = steep
 emtol                    = 100.0
 emstep                   = 0.01
-nsteps                   = 500 ; 100000
+nsteps                   = 1000 ; 100000
 cutoff-scheme            = Verlet
 energygrps               = System
 nstlist                  = 10
@@ -197,17 +206,17 @@ os.system( 'gmx grompp -c {out_grofile} -f {out_rundir}/quickmin.mdp -p {out_top
 os.system( 'gmx mdrun -v -s {out_rundir}/min.tpr -c {out_rundir}/npt.gro'.format(out_rundir=out_rundir) )
 
 # print some commands to test the script
-print()
-print('### To test this project, try: ###')
-print("""
-python ../scripts/create_ee_mdp.py {out_rundir}/npt.gro {out_topfile} {ndxfile} {out_rundir}/prod.mdp
-mkdir test
+testing_cmds = "### To test this project, try: ###\n"
+
+if ligand_only:
+    testing_cmds += "python ../scripts/create_ee_mdp.py {out_rundir}/npt.gro {out_topfile} {ndxfile} {out_rundir}/prod.mdp ligonly\n".format(out_topfile=out_topfile, ndxfile=ndxfile, out_rundir=out_rundir)
+
+else:
+    testing_cmds += "python ../scripts/create_ee_mdp.py {out_rundir}/npt.gro {out_topfile} {ndxfile} {out_rundir}/prod.mdp\n".format(out_topfile=out_topfile, ndxfile=ndxfile, out_rundir=out_rundir)
+
+testing_cmds += """mkdir test
 gmx grompp -c {out_rundir}/npt.gro -f {out_rundir}/prod.mdp -p {out_topfile} -n {ndxfile} -o test/testme.tpr -po mdout.mdp -maxwarn 1
 cd test
-gmx mdrun -v -s testme.tpr
-""".format(out_grofile=out_grofile, out_topfile=out_topfile, ndxfile=ndxfile, out_rundir=out_rundir) )
+gmx mdrun -v -s testme.tpr""".format(out_topfile=out_topfile, ndxfile=ndxfile, out_rundir=out_rundir) 
 
-
-
-
-
+print(testing_cmds)
