92a9005b8e99b4f58af33833de2cacdf85e2a38a
yabmtm <tug27224@temple.edu>
2020-04-16 00:35:12 -0400

    Updated and final (for now) scrape/analyze.py

--- FEP/analysis/analyze.py
+++ FEP/analysis/analyze.py
@@ -3,126 +3,207 @@
 import pandas as pd
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-import tqdm, sys, glob
+import tqdm, sys, glob, datetime, os
+import re, subprocess
 
 # in columns = [description  project  run  clone  gen  wl_increment  lam_0 ... lam38 free energy]
 # out columns [run  ns_RL  ns_L  wl_increment_RL  wl_increment_L  free_energy  L39..L0  RL0..R39]
+# Sample input: Ligset_start#_end# < this is the description variable
 
-max_gen = True
-data = []
-description = '72_L_101_200'
-data_RL = pd.read_pickle(glob.glob('avg_72_RL_101_200_*.pkl')[0])
-data_L = pd.read_pickle(glob.glob('avg_72_L_101_200_*.pkl')[0])
-lambdas = 40
-RL_gen_cutoff = 15 # only process clones that have more than N ns of sampling
-L_gen_cutoff = 10
-wl_increment_cutoff = 0.2 # only plot ligands with avg_RL_WL_increment < N
-skip_positive_G = True
-columns = ['run', 'ns_RL', 'ns_L', 'wl_increment_RL', 'wl_increment_L', 'free_energy'] + [f'L{lam}' for lam in reversed(range(lambdas))] + [f'RL{lam}' for lam in range(lambdas)]
-runs = min([data_RL['run'].max(), data_L['run'].max()]) + 1 # only process runs that have data for both RL and L (this can be done better)
-data, errors = [],[]
+description = sys.argv[1] # should be like MS0326_1-1500 or AGG_10000-12000
+project = sys.argv[2] # project number assoicated with RL-system matching description
+lambdas = 40 # 20 lambdas to (de)couple each of coulomb/vdw == 40 total
 
+# constraints
+RL_gen_cutoff = 10 # only process clones that have more than N ns of sampling
+L_gen_cutoff = 5
+wl_increment_cutoff = 0.25 # only plot ligands with avg_RL_WL_increment < N
+clone_cutoff = 2 # only include runs that have >=N clones that satisfy restraints (use 1-3)
 
-for run in tqdm.tqdm(range(runs)):
+# get FAH data directory from hostname
+hostname = subprocess.check_output('hostname', shell=True).decode().strip('\n')
+hostname_paths = {'vav3.ocis.temple.edu':'/home/server/server2/data/SVR166219',
+                  'vav4.ocis.temple.edu':'/home/server/server2/data/SVR166220',
+                  'vav15.ocis.temple.edu':'/data/SVR2616698069',
+                  'vav16.ocis.temple.edu':'/data/SVR2616698070',
+                  'folder-prod-001':'/data/SVR1163805190', #avast1
+                  'folder-prod-002':'/data/SVR1163805191', #avast2
+                  'fah5':'/data/SVR679057516',
+                  'aws2':'/data/SVR51748107'}
+server_path = f'{hostname_paths[hostname]}/PROJ{project}'
+
+try:
+    desc = re.split(r'[_-]', f'{description}')
+    data_RL_file = max(glob.glob(f'scraped_data/{desc[0]}_RL_{desc[1]}*{desc[2]}*.pkl'), key=os.path.getctime)
+    data_L_file = max(glob.glob(f'scraped_data/{desc[0]}_L_{desc[1]}*{desc[2]}*.pkl'), key=os.path.getctime)
+    data_RL = pd.read_pickle(data_RL_file)
+    data_L = pd.read_pickle(data_L_file)
+    whole_dataset = pd.read_pickle(f'{desc[0]}.pkl') # master dataframe for dataset (ask matt if missing)
+    print(f'Using most recent data files:\n\tRL Data: {data_RL_file}\n\tL Data: {data_L_file}')
+#    print(f'{data_RL}\n{data_L}')
+except IndexError as e:
+    print(f'No pickled dataset found for description: {description}')
+
+# make sure output directories are present
+for dir in ['plots','results']:
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+## scrape pull info
+temperature = 300.0
+kB = 1.381e-23 * 6.022e23 / 1000.0 # Boltzmann constant in kJ/mol/K
+beta = 1.0 / (kB * temperature) # inverse temperature of simulations (in 1/(kJ/mol))
+with open(f'/home/server/server2/projects/Gromacs/p{project}/RUN0/prod.mdp') as f:
+    lines = [line.strip('\n').split() for line in f.readlines()]
+for line in lines:
+    try:
+        if 'pull-coord1-init' in line[0]:
+            equilibrium_distance = float(line[2])
+        elif 'pull_coord1_k' in line[0]:
+            kspring = float(line[2])
+    except Exception as e:
+        continue
+
+# process runs that have data for both RL/L
+runs = set(list(data_RL['run'].values) + list(data_L['run'].values))
+data, errors, clone_energies = [],[],[]
+for run in tqdm.tqdm(runs):
     run_RL = data_RL.loc[data_RL['run'] == run]
     run_L = data_L.loc[data_L['run'] == run]
 
-    # process L systems
+    # process L systems for last gen of each clone
     energies_L, wl_increment_L, ns_L = [],[],[]
     for clone_L in set(run_L['clone'].values):
         try:
             clone_L_data = run_L.loc[run_L['clone'] == clone_L]
             gen_L = clone_L_data.loc[clone_L_data['gen'] == clone_L_data['gen'].max()]
-            if clone_L_data['gen'].max() < L_gen_cutoff: # we skip clones with fewer than N gens
-                continue
+            if clone_L_data['gen'].max() < L_gen_cutoff:
+                continue # skip clones with fewer than N gens or HIGH WL-increment
             wl_increment_L.append(gen_L['wl_increment'].values[0])
-            ns_L.append(int(gen_L['gen'].values[0]))
+            ns_L.append(10+int(gen_L['gen'].values[0])*10)
             raw_energies = gen_L[gen_L.columns[-lambdas:]].values[0]
-            energies_L.append([x - raw_energies[0] for x in raw_energies])
+            energies_L.append([float(x) for x in raw_energies]) #- raw_energies[0] for x in raw_energies])
+            clone_energies.append([run, 'L', ns_L[-1], wl_increment_L[-1], list(reversed(energies_L[-1]))])
         except Exception as e:
             print(f'L Exception: {run}, {clone_L}, {e}')
             continue
     try:
         avg_WL_increment_L = np.average(wl_increment_L)
-        std_WL_increment_L = np.std(wl_increment_L)
         avg_ns_L = np.average(ns_L)
-        std_ns_L = np.std(ns_L)
         avg_L_energies = [np.average(np.asarray(energies_L)[:,lam]) for lam in range(lambdas)]
-        std_L_energies = [np.std(np.asarray(energies_L)[:,lam]) for lam in range(lambdas)]
     except Exception as e:
         continue
 
-    # process RL systems
-    energies_RL, wl_increment_RL, ns_RL = [],[],[]
+    # process RL systems for last gen of each clone
+    energies_RL, wl_increment_RL, ns_RL, pull_energies = [],[],[],[]
     for clone_RL in set(run_RL['clone'].values):
         try:
             clone_RL_data = run_RL.loc[run_RL['clone'] == clone_RL]
             gen_RL = clone_RL_data.loc[clone_RL_data['gen'] == clone_RL_data['gen'].max()]
-            if clone_RL_data['gen'].max() < RL_gen_cutoff: # we skip clones with fewer than N gens
-                continue
+            if clone_RL_data['gen'].max() < RL_gen_cutoff or clone_RL_data['wl_increment'].min() > wl_increment_cutoff:
+                continue # skip clones with fewer than N gens or HIGH WL-increment
             wl_increment_RL.append(gen_RL['wl_increment'].values[0])
-            ns_RL.append(int(gen_RL['gen'].values[0]))
+            ns_RL.append(int(gen_RL['gen'].values[0])+1)
             raw_energies = gen_RL[gen_RL.columns[-lambdas:]].values[0]
-            energies_RL.append([x - raw_energies[0] for x in raw_energies])
+            energies_RL.append([float(x) for x in raw_energies])
+            clone_energies.append([run, 'RL', ns_RL[-1], wl_increment_RL[-1], energies_RL[-1]])
+            # look at harmonic restraint of last gen, for each clone that fits the constraints
+ #           with open(f"{server_path}/RUN{run}/CLONE{clone_RL}/results{int(gen_RL['gen'].values[0])}/pullx.xvg") as xvg:
+ #               lines = [ line.strip('\n').split() for line in xvg.readlines()][20:][::10]
+ #           displacements = [np.sqrt(float(line[1])**2 + float(line[2])**2 + float(line[3])**2) for line in lines]
+            pull_energies.append([]) #displacements) # only look at displacements for v1
+#            pull_energies.append([beta*(kspring/2.0)*(displacement - equilibrium_distance)**2 for displacement in displacements])
+
         except Exception as e:
             print(f'RL Exception: {run}, {clone_RL}, {e}')
             continue
 
     try:
         avg_WL_increment_RL = np.average(wl_increment_RL)
-        std_WL_increment_RL = np.std(wl_increment_RL)
         avg_ns_RL = np.average(ns_RL)
-        std_ns_RL = np.std(ns_RL)
         avg_RL_energies = [np.average(np.asarray(energies_RL)[:,lam]) for lam in range(lambdas)]
-        std_RL_energies = [np.std(np.asarray(energies_RL)[:,lam]) for lam in range(lambdas)]
         avg_free_energy = avg_RL_energies[-1] - avg_L_energies[-1]
-        std_free_energy = np.sum(std_RL_energies + std_L_energies)
-
         data.append([run, avg_ns_RL, avg_ns_L, avg_WL_increment_RL, avg_WL_increment_L, avg_free_energy] + list(reversed(avg_L_energies)) + list(avg_RL_energies))
-        errors.append([run, std_ns_RL, std_ns_L, std_WL_increment_RL, std_WL_increment_L, std_free_energy] + list(reversed(std_L_energies)) + list(std_RL_energies))
-
     except Exception as e:
         continue
 
+columns = ['run', 'ns_RL', 'ns_L', 'wl_increment_RL', 'wl_increment_L', 'free_energy'] + [f'L{lam}' for lam in reversed(range(lambdas))] + [f'RL{lam}' for lam in range(lambdas)]
+clone_energies_columns = ['run', 'type', 'length', 'wl_increment', 'energies']
 results = pd.DataFrame(data, columns=columns)
-errors = pd.DataFrame(errors, columns=columns)
-results.to_pickle(f'{description}_results.pkl')
-errors.to_pickle(f'{description}_errors.pkl')
+clone_energies = pd.DataFrame(clone_energies, columns=clone_energies_columns)
+whole_project = whole_dataset.loc[whole_dataset['project'] == int(project)]
+np.save(f'pull_{description}.npy',pull_energies)
 
-good_results = results.loc[results['wl_increment_RL'] < wl_increment_cutoff]
-good_results_errors = errors.loc[results['wl_increment_RL'] < wl_increment_cutoff]
-
-if skip_positive_G:
-    good_results_errors = good_results_errors.loc[good_results['free_energy'] < 0]
-    good_results = good_results.loc[good_results['free_energy'] < 0]
-
-print(f'*** Results based on RL sampling > {RL_gen_cutoff} ns, L sampling > {L_gen_cutoff} ns, and a RL WL-increment < {wl_increment_cutoff}:')
-print(good_results)
-
-### example plots
-for run in good_results['run'].values:
+print(f'*** Results are based on RL sampling > {RL_gen_cutoff} ns, L sampling > {L_gen_cutoff} ns, and a RL WL-increment < {wl_increment_cutoff}:')
+### Saving Plots and Dataframe
+good_results = []
+good_results_columns = ['dataset','fah','identity','receptor','score','febkT','error','ns_RL','ns_L','wl_RL']
+for run in tqdm.tqdm(results['run'].values):
     try:
-        result = good_results.loc[good_results['run'] == run]
-        result_error = good_results_errors.loc[good_results['run'] == run]
-        print(f"Plotting RUN{run}\nRL_WL_increment: {result['wl_increment_RL'].values[0]:.3f}\nΔG_unb = {result['free_energy'].values[0]:.3f}kT\n")
+        run_info = whole_project.loc[whole_project['run'] == run]
+        clones_RL = clone_energies.loc[(clone_energies['run'] == run) & (clone_energies['type'] == 'RL')]
+        clones_L = clone_energies.loc[(clone_energies['run'] == run) & (clone_energies['type'] == 'L')]
+        result = results.loc[results['run'] == run]
         summary = list(result[result.columns[:6]].values[0]) # run ns_RL, ns_L, wl_increment_RL, wl_increment_L
-        summary_error = list(result_error[result_error.columns[:6]].values[0])
-
         energies = result[result.columns[6:]].values[0]
-        energies = [x - energies[0] for x in energies] # set first point to 0
-        energy_errors = result_error[result_error.columns[6:]].values[0]
-        energy_errors = [sum(energy_errors[0:x + 1]) for x in range(len(energy_errors))]
+        energies = [x - energies[0] for x in energies] # set first point to 0kT reference
 
-        plt.scatter(range(len(energies)), energies)
-        plt.plot(range(len(energies)), energies)
-        plt.errorbar(range(len(energies)), energies, yerr=energy_errors)
-        plt.title(f'MS0323_RUN{summary[0]}: ΔG = {summary[-1]:.3f} ± {summary_error[-1]:.3f}kT')  
-        plt.xlabel('Ligand Decoupling --> Receptor-Ligand Coupling')
+        if len(clones_RL) < clone_cutoff: # skip runs which have fewer RL clones than the clone_cutoff
+            continue
+
+        clone_combinations = []
+        fig, ax = plt.subplots()
+        for lindex, lrow in clones_L.iterrows():
+            for rindex, rrow in clones_RL.iterrows():
+                clone_energy = lrow['energies'] + rrow['energies']
+                clone_energy = [x - clone_energy[0] for x in clone_energy]
+                ax.plot(range(len(clone_energy)), clone_energy)
+                clone_combinations.append(clone_energy)
+        energy_errors = np.std(clone_combinations, axis=0)
+        ax.errorbar(range(len(energies)), energies, yerr=energy_errors)
+        ax.axhline(0,linestyle='--') # draw horizontal lines at reference
+        ax.axhline(summary[-1],linestyle='--') # and at free energy
+        if result['wl_increment_RL'].values[0] > 0.2: # color complex lambdas based on WL-increment
+            wl_color = 'red'
+        elif result['wl_increment_RL'].values[0] > 0.1:
+            wl_color = 'yellow'
+        else:
+            wl_color = 'green'
+        ax.axvspan(0, lambdas-1, alpha=0.1, color='blue') # color first half of lambdas blue
+        ax.axvspan(lambdas-1, lambdas*2, alpha=0.1, color=wl_color) # color second half of lambdas (consider changing this based on some other metric.)
+        if summary[-1] < 0: # change color of horizontal free energy strip depending on sign
+            nrg_color = 'green'
+        else:
+            nrg_color = 'red'
+        ax.axhspan(0,summary[-1], alpha=0.5, color=nrg_color)
+        ax.set_title(f"{desc[0]}: p{run_info['project'].values[0]}:R{run_info['run'].values[0]}, {run_info['identity'].values[0]}, ΔG = {summary[-1]:.3f}±{energy_errors[-1]:.2f}kT")
+        plt.xlim(0,80)
+        plt.xlabel('Ligand Decoupling → Receptor-Ligand Coupling')
         plt.ylabel('Free Energy (kT)')
-        plt.savefig(f'FEP_{run}.png')
-#        plt.close()
+        date = datetime.datetime.now()
+        ts = date.strftime('%d%b%Y')
+        plt.savefig(f'plots/{description}_{run}_{ts}.png')
+        plt.close()
+
+        good_results.append([desc[0],f"{server_path}/PROJ{run_info['project'].values[0]}/RUN{run_info['run'].values[0]}",run_info['identity'].values[0],run_info['receptor'].values[0],run_info['score'].values[0], result['free_energy'].values[0], energy_errors[-1], clones_RL['length'].values, clones_L['length'].values, clones_RL['wl_increment'].values])
+
     except Exception as e:
         print(f'Exception in computing energies/errors and plotting: {e}')
         continue
+
+good_results = pd.DataFrame(good_results, columns=good_results_columns)
+good_results = good_results.sort_values(by=['febkT'])
+print(good_results)
+good_results.to_pickle(f'results_{description}.pkl')
+
+# stuff I took out of plotting
+#    results.to_pickle(f'results_{description}.pkl')
+#    clone_energies.to_pickle(f'clone_energies_{description}.pkl')
+#        print(f"Plotting RUN{run}\nRL_WL_increment: {result['wl_increment_RL'].values[0]:.3f}\nΔG_unb = {result['free_energy'].values[0]:.3f}±{energy_errors[-1]:.3f}kT\n")
+#        ax.set_title(f'{desc[0]}_{run}: RL > {RL_gen_cutoff} ns, L > {L_gen_cutoff} ns, and RL-WLI < {wl_increment_cutoff:.3f}') #_RUN{summary[0]}: ΔG = {summary[-1]:.2f} ± {summary_error[-1]:.2f}kT')
+#        label = f'RL/L: RUN{run} {int(summary[1])}/{int(summary[2])}ns {summary[3]:.3f}/{summary[4]:.3f}WL, ΔG = {summary[-1]:.3f}±{energy_errors[-1]:.2f}kT'
+#        ax.annotate(label, xy = (range(len(energies))[-1], energies[-1]), xytext = (60, 100),
+#              textcoords = 'offset points', ha = 'right', va = 'bottom',
+#              bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+#                  arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
--- FEP/analysis/scrape.py
+++ FEP/analysis/scrape.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
-
+# Imports
 import os, sys, re, subprocess, tqdm
 import numpy as np
 import pandas as pd
 import datetime
+import glob
 
-project = sys.argv[1]
-description = sys.argv[2]
-columns = ['description','SMILES','project','run','clone','gen','wl_increment'] + [f'lam_{x}' for x in range(39)] + ['free_energy']
+# Example input
+#python scrape.py 14601 72_RL_101_200 > creates 72_RL_101_200_date/time.pkl
+ 
+# Arguments
+project = sys.argv[1]                                             # Project Number
+description = sys.argv[2]                                         # Description -i.e. Ligand_Set_L/RL_start_end
+whole_dataset = pd.read_pickle(f'MS03262020_1-5660.pkl') # master dataframe for dataset (ask matt if missing) 
+whole_project = whole_dataset.loc[whole_dataset['project'] == int(project)]  # extract info for project being scraped
+
+try:                                                              # This loads in the most recently scraped df if it exists 
+    previous_df = pd.read_pickle(max(glob.glob(f'scraped_data/{description}*') , key=os.path.getctime))
+    chk_df = previous_df.filter(['run','clone','gen'])  # This will pull out run, clone, gen from old df
+except Exception as e:                                            # if there is no old df...
+    previous_df,chk_df = None, None
+columns = ['description','identity','project','run','clone','gen','wl_increment'] + [f'lam_{x}' for x in range(39)] + ['free_energy'] # Columns for df
+
+if not os.path.exists('scraped_data'):
+    os.makedirs('scraped_data')
 
 # pull some info out of project.xml
 with open(f'/home/server/server2/projects/Gromacs/p{project}/project.xml') as f:
@@ -25,52 +41,57 @@ for line in lines:
     except Exception as e:
         continue
 
-path = f'../SVR2616698070/PROJ{project}'
-if sys.argv[3] != None:
-    previous_df = pd.read_pickle(sys.argv[3])
-    chk_df = previous_df.filter(['run','clone','gen'])
-else:
-    pass
-
+# server-dependent variables
+hostname_paths = {'vav3.ocis.temple.edu':'/home/server/server2/data/SVR166219',
+                  'vav4.ocis.temple.edu':'/home/server/server2/data/SVR166220',
+                  'vav15.ocis.temple.edu':'/data/SVR2616698069',
+                  'vav16.ocis.temple.edu':'/data/SVR2616698070',
+                  'folder-prod-001':'/data/SVR1163805190', #avast1
+                  'folder-prod-002':'/data/SVR1163805191', #avast2
+                  'fah5':'/data/SVR679057516',
+                  'aws2':'/data/SVR51748107'}
+hostname = subprocess.check_output('hostname', shell=True).decode().strip('\n')
+path = f'{hostname_paths[hostname]}/PROJ{project}'
+    
+# scrape the new data
 data = []
 for run in tqdm.tqdm(range(runs)):
+    run_info = whole_project.loc[whole_project['run'] == run]
     for clone in range(clones):
         for gen in range(gens):
-            if not os.path.exists(f'{path}/RUN{run}/CLONE{clone}/results{gen}/md.log'):
+            if not os.path.exists(f'{path}/RUN{run}/CLONE{clone}/results{gen}/md.log'):    # This breaks the loop if the gen does not exist
                 break
-            elif sys.argv != None: 
+            if chk_df is not None:                                                       # This checks if run,clone,gen exists in old df
                 if ((chk_df['run'] == run) & (chk_df['clone'] == clone) & (chk_df['gen'] == gen)).any() == True:
                     continue
             else:
                 pass
-# this averages the last 100 entries in md.log
-            cmd = f'tail -n 6208 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep -B 39 "40  0.000  1.000  1.000"'
-            log = [line.split() for line in subprocess.check_output(cmd, shell=True).decode().split('\n')[:-1]]
-            free_energies = [np.average([float(line[5]) for line in log if line[0] == str(lam)]) for lam in range(1,41)]
+            # this averages the last 100 entries in md.log
+            try:
+                cmd = f'tail -n 6208 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep -B 39 "40  0.000  1.000  1.000"'
+                log = [line.split() for line in subprocess.check_output(cmd, shell=True).decode().split('\n')[:-1]]
+                free_energies = [np.average([float(line[5]) for line in log if line[0] == str(lam)]) for lam in range(1,41)]
+            except Exception as e:
+                print(f'Bad md.log file! {path}/RUN{run}/CLONE{clone}/results{gen}/md.log. Skipping to next gen, I guess?')
 
-#            cmd = f'tail -n 6208 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep "increment"'
-#            log = [line.split() for line in subprocess.check_output(cmd, shell=True).decode().split('\n')[:-1]]
-#            wc_increment = np.average([float(line[3]) for line in log])
+            # SMILES...yr on Camera ( writes SMILES string or identity)  
+            identity = run_info['identity'].values[0]
 
-# SMILES...yr on Camera ( writes SMILES string)  
-            cmd = f'head -n1 ../projects/p{project}/RUN{run}/*_h.sdf'
-            SMILES = subprocess.check_output(cmd,shell=True)
+            try: # get WL-increment at end of md.log
+                cmd = f'tail -n 1000 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep "increment" | tail -n 1'
+                wl_increment = float(subprocess.check_output(cmd, shell=True).decode().split()[3])
+                data.append([description, identity, project, run, clone, gen, wl_increment] + free_energies)
+            except Exception as e:
+                print(f'Bad md.log file2! {path}/RUN{run}/CLONE{clone}/results{gen}/md.log. Skipping to next gen, I guess?')
+                continue
 
-# this uses just the last entry of md.log
-#            cmd = f'tail -n 1000 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep -B 40 "40  0.000  1.000  1.000" | tail -n 40'
-#            free_energies = [line.split() for line in subprocess.check_output(cmd, shell=True).decode().split('\n')[:-1]]
-
-            cmd = f'tail -n 1000 {path}/RUN{run}/CLONE{clone}/results{gen}/md.log | grep "increment" | tail -n 1'
-            wl_increment = float(subprocess.check_output(cmd, shell=True).decode().split()[3])
-            data.append([description, SMILES, project, run, clone, gen, wl_increment] + free_energies)
-
-if sys.argv[3] != None:
-    df = previous_df.append(pd.DataFrame(data, columns=columns), ignore_index=True)
-    df = df.sort_values(['run', 'clone', 'gen'], ascending=[True, True, True])
-    df = df.reset_index()
-else:
-    df = pd.DataFrame(data, columns=columns)
+if chk_df is not None:
+    df = previous_df.append(pd.DataFrame(data, columns=columns), ignore_index=True)     # This appends old df with new info
+    df = df.sort_values(['run', 'clone', 'gen'], ascending=[True, True, True])          # This sorts run,clone,gen in sequential order
+    df = df.reset_index(drop=True)                                                      # This resets the index to be sequential
+else:                                                                                   
+    df = pd.DataFrame(data, columns=columns)                                            # If no old df, make new one 
 datetime_object = datetime.datetime.now()
 ts = datetime_object.strftime("%d%b%Y_%H-%M-%S")
-df.to_pickle(f'avg_{description}_{ts}.pkl')
+df.to_pickle(f'scraped_data/{description}_{ts}.pkl')
 
