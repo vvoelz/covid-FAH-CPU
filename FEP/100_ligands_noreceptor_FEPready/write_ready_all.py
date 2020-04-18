7978464babb7ab0dfd79ac55d5be28598b1ee578
Vincent Voelz <vvoelz@gmail.com>
2020-04-18 12:36:50 -0400

    update

--- FEP/100_ligands_noreceptor_FEPready/write_ready_all.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-for run in range(1,101):
-    print("python ../scripts/make_fep_ready.py ../100_ligands_noreceptor/RUN%d ./RUN%d ligonly"%(run,run) )
-    print("# cleanup!")
-    print("rm ./#* ;  rm step*")
