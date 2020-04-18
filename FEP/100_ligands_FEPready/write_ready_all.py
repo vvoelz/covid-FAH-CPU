7b36e4268cc44c9c9422e5c4c0009d9988f19318
vvoelz <vvoelz@gmail.com>
2020-04-18 11:24:17 -0400

    got rid of old prelim dirs

--- FEP/100_ligands_FEPready/write_ready_all.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-for run in range(1,99):
-    print("python make_fep_ready.py ../100_ligands/RUN%d ./RUN%d"%(run,run) )
-    print("# cleanup!")
-    print("rm ./#* ;  rm step*")
--- FEP/test_set/do.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..100}; do
-    mkdir LIG$i
-    cp results$i.mol2 LIG$i
-    cp results$i.pdb LIG$i
-    obabel -imol2 LIG$i/results$i.mol2 -osdf > LIG$i/results$i.sdf; done
